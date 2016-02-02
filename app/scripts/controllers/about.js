a72dd72d5278a8b6ccc05c96f1a89478a2660b01
dinaerdianinque <ohwellaye@gmail.com>
2016-02-01 17:19:41 -0800

    setting up

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name badgesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the badgesApp
+ */
+angular.module('badgesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
